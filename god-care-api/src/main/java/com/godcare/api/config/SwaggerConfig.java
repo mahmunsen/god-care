a429a3337403ef6090b2c5e0d0b9fd3dd090fc02
mahmunsen <tjs339@gmail.com>
2024-03-04 14:35:33 +0900

    #2 [chore] .gitignore 수정, 아래 코드들 삭제 HELP.md out/ !**/src/main/**/out/ !**/src/test/**/out/

--- god-care-api/src/main/java/com/godcare/api/config/SwaggerConfig.java
+++ god-care-api/src/main/java/com/godcare/api/config/SwaggerConfig.java
@@ -2,28 +2,27 @@ package com.godcare.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.github.godcare.controller")) // //  Swagger API 문서로 만들기 원하는 BasePackage 경로. (필수)
+                .apis(RequestHandlerSelectors.basePackage("com.godcare.api.controller")) //  Swagger API 문서로 만들기 원하는 BasePackage 경로. (필수)
                 .paths(PathSelectors.any()) // URL 경로를 지정하여 해당 URL에 해당하는 요청만 Swagger API 문서로 만든다. (any()로 설정 시 모든 api가 보여짐, (필수))
                 .build()
-                .apiInfo(apiInfo()) // Swagger API 문서에 대한 설명을 표기하는 메소드. (선택)
-                .consumes(Collections.singleton(MediaType.MULTIPART_FORM_DATA_VALUE)) // 서버로 보낼 데이터의 Content-type을 설정
-                .produces(Collections.singleton(MediaType.APPLICATION_JSON_VALUE)); // 클라이언트에게 전송할 데이터의 Content-type을 설정
+                .apiInfo(apiInfo()); // Swagger API 문서에 대한 설명을 표기하는 메소드. (선택)
     }
 
     private ApiInfo apiInfo() {
