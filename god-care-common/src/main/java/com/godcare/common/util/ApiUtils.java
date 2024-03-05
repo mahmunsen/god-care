4771eba26e8569e8fa2a9947feb975d7d2e93582
mahmunsen <tjs339@gmail.com>
2024-03-05 16:37:32 +0900

    #3 [feat] ApiUtils 파일 수정 - 안쓰이는 success 메소드 제거 - ResponseEntity 형태로 반환되게끔 수정

--- god-care-common/src/main/java/com/godcare/common/util/ApiUtils.java
+++ god-care-common/src/main/java/com/godcare/common/util/ApiUtils.java
@@ -1,16 +1,11 @@
 package com.godcare.common.util;
 
 import com.godcare.common.vo.Response;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 public class ApiUtils {
 
-    public static <T> Response<T> success(Integer status, String message, T data) {
-        return new Response<>(true, status, message, data);
+    public static <T> ResponseEntity<Response<T>> success(Integer status, String message, T data) {
+        return ResponseEntity.status(status).body(new Response<>(true, status, message, data));
     }
-
-    public static <T> Response<T> success(HttpStatus status, String message, T data) {
-        return new Response<>(true, status.value(), message, data);
-    }
-
 }
