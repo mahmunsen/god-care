51515721a9c609a9e8503acd95c5a814abc5647a
mahmunsen <tjs339@gmail.com>
2024-03-10 22:29:46 +0900

    #1 [chore] api 모듈 ApiApplication 어노테이션 수정 - @EnableSwagger2 -> SwaggerConfig로 옮김 - @EntityScan제거

--- god-care-api/src/main/java/com/godcare/api/ApiApplication.java
+++ god-care-api/src/main/java/com/godcare/api/ApiApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+
 @SpringBootApplication(scanBasePackages = {"com.godcare.api", "com.godcare.common"})
-@EntityScan("com.godcare.core")
 public class ApiApplication {
 
     public static void main(String[] args) {
