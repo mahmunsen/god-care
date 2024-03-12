773151150cc477d1c337158c3037ca2c394e6b5c
mahmunsen <tjs339@gmail.com>
2024-03-12 12:56:03 +0900

    #4 [feat] ProductService 작성 - 상품등록, 조회, 전체조회, 수정 및 삭제

---
+++ god-care-api/src/main/java/com/godcare/api/service/ProductService.java
@@ -0,0 +1,49 @@
+package com.godcare.api.service;
+
+import com.godcare.api.entity.Product;
+import com.godcare.api.repository.ProductRepository;
+import com.godcare.common.dto.UpdateProductRequest;
+import com.godcare.common.dto.ViewProductResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    // 상품등록
+    public Product addProduct(Product product) {
+        Product savedProduct = productRepository.save(product);
+        return savedProduct;
+    }
+    // 상품조회
+    public ViewProductResponse getProduct(Long productId) {
+        Product product = productRepository.findById(productId);
+        return new ViewProductResponse(product.getId(), product.getMainImg(), product.getCategoryId());
+    }
+    // 상품전체조회
+    public List<ViewProductResponse> getAllProducts() {
+        Collection<Product> values = productRepository.values();
+        return values.stream().map(product -> new ViewProductResponse(product.getId(), product.getMainImg(), product.getCategoryId())).collect(Collectors.toList());
+    }
+    // 상품수정
+    public Product updateProduct(Long productId, UpdateProductRequest updateProductRequest) {
+        // 상품 찾기
+        Product product = productRepository.findById(productId);
+        // 해당 엔티티의 내용을 수정 사항과 맵핑하여 저장
+        Product updatedProduct = productRepository.saveUpdated(product.toUpdatedProduct(updateProductRequest, product.getId()));
+        return updatedProduct;
+    }
+
+    // 상품삭제
+    public void deleteProduct(Long productId) {
+        productRepository.deleteById(productId);
+    }
+}
