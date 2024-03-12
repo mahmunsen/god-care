055e51c310945be801e2aa124bb4463ddd657915
mahmunsen <tjs339@gmail.com>
2024-03-12 12:52:43 +0900

    #0 [docs] SwaggerConfig documentationType 수정

--- god-care-api/src/main/java/com/godcare/api/config/SwaggerConfig.java
+++ god-care-api/src/main/java/com/godcare/api/config/SwaggerConfig.java
@@ -18,9 +18,9 @@ import java.util.Collections;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.github.godcare.controller")) // //  Swagger API 문서로 만들기 원하는 BasePackage 경로. (필수)
+                .apis(RequestHandlerSelectors.basePackage("com.godcare.api.controller")) // //  Swagger API 문서로 만들기 원하는 BasePackage 경로. (필수)
                 .paths(PathSelectors.any()) // URL 경로를 지정하여 해당 URL에 해당하는 요청만 Swagger API 문서로 만든다. (any()로 설정 시 모든 api가 보여짐, (필수))
                 .build()
                 .apiInfo(apiInfo()) // Swagger API 문서에 대한 설명을 표기하는 메소드. (선택)
