a9d44d7d652c4c98599bb29729132c514154f4c4
mahmunsen <tjs339@gmail.com>
2024-03-19 13:02:39 +0900

    #3 [refactor] ErrorCode의 getCode() 수정 - int -> HttpStatus

--- god-care-api/src/main/java/com/godcare/api/exception/ErrorCode.java
+++ god-care-api/src/main/java/com/godcare/api/exception/ErrorCode.java
@@ -1,6 +1,9 @@
 package com.godcare.api.exception;
 
+import org.springframework.http.HttpStatus;
+
+
 public interface ErrorCode {
-    int getCode();
+    HttpStatus getCode();
     String getMessage();
 }
