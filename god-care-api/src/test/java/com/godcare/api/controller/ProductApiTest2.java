cbc5f90670a8ff68f9286b33845b4acc2fde9a28
mahmunsen <tjs339@gmail.com>
2024-03-19 21:17:43 +0900

    #4 [feat] 상품 수정 테스트 생성

--- god-care-api/src/test/java/com/godcare/api/controller/ProductApiTest2.java
+++ god-care-api/src/test/java/com/godcare/api/controller/ProductApiTest2.java
@@ -6,6 +6,7 @@ import com.godcare.api.entity.Product;
 import com.godcare.api.repository.ProductRepository;
 import com.godcare.api.service.ProductService;
 import com.godcare.api.vo.Response;
+import com.godcare.common.dto.UpdateProductRequest;
 import com.godcare.common.dto.ViewProductResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,10 +24,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -49,7 +53,7 @@ public class ProductApiTest2 {
     @DisplayName("상품을 단건 조회한다.")
     void viewProduct() throws Exception {
         // given
-        Long id = 3L;
+        Long productId = 3L;
 
         Product product = Product.builder()
                 .id(3L)
@@ -67,7 +71,7 @@ public class ProductApiTest2 {
         when(productService.getProduct(anyLong())).thenReturn(viewProductResponse);
 //        when(productRepository.findById(anyLong())).thenReturn(product);
 
-        MvcResult mvcResult = mvc.perform(get("/api/v1/products/{product_id}", id))
+        MvcResult mvcResult = mvc.perform(get("/api/v1/products/{product_id}", productId))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andReturn();
@@ -79,7 +83,7 @@ public class ProductApiTest2 {
         assertEquals(response.getData().getProductId(), 3L);
         assertEquals(response.getData().getMainImg(), "ex.img");
         assertEquals(response.getData().getCategoryId(), 4L);
-        verify(productService).getProduct(id);
+        verify(productService).getProduct(productId);
 //        verify(productRepository).findById(id);  // 작동하지 않음
     }
 
@@ -106,6 +110,42 @@ public class ProductApiTest2 {
         assertEquals(response.getData().get(2).getMainImg(),"probiotics");
         assertEquals(response.getData().get(3).getMainImg(),"star-care");
         verify(productService).getAllProducts();
+    }
+
+
+    @Test
+    @DisplayName("해당 상품을 수정한다.")
+    void updateProduct() throws Exception {
+        // given
+        Long productId = 4L;
+
+        UpdateProductRequest request = new UpdateProductRequest("god-care.img", 3L);
+        Product product = Product.builder()
+                .mainImg(request.getMainImg())
+                .categoryId(request.getCategoryId())
+                .id(productId)
+                .build();
+
+        // when, then
+        when(productService.updateProduct(anyLong(),any(UpdateProductRequest.class))).thenReturn(product);
+
+        // 요청으로 보낼 dto
+        String content = new ObjectMapper().writeValueAsString(request);
+
+        MvcResult mvcResult = mvc.perform(patch("/api/v1/products/{product_id}", productId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        Response<String> response = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<Response<String>>() {
+        });
+
+        assertEquals(response.getSuccess(), true);
+        assertEquals(response.getData(), "productId: " + product.getId());
+
+        verify(productService).updateProduct(productId, request);
     }
 }
 
--- god-care-common/src/main/java/com/godcare/common/dto/UpdateProductRequest.java
+++ god-care-common/src/main/java/com/godcare/common/dto/UpdateProductRequest.java
@@ -1,10 +1,8 @@
 package com.godcare.common.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@EqualsAndHashCode
 @Builder
 @Getter
 @NoArgsConstructor
