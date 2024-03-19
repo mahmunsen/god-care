605e2da40857756d5adc65451bb836acbf1d6c8f
mahmunsen <tjs339@gmail.com>
2024-03-19 19:53:11 +0900

    #4 [refactor] ProductController 수정 - ApiUtils 제거 - @ResponseStatus 사용

--- god-care-api/src/main/java/com/godcare/api/controller/ProductController.java
+++ god-care-api/src/main/java/com/godcare/api/controller/ProductController.java
@@ -2,18 +2,16 @@ package com.godcare.api.controller;
 
 import com.godcare.api.service.ProductService;
 import com.godcare.common.dto.ResisterProductRequest;
-import com.godcare.api.util.ApiUtils;
 import com.godcare.api.vo.Response;
 import com.godcare.api.entity.Product;
+import com.godcare.common.dto.ResisterProductResponse;
 import com.godcare.common.dto.UpdateProductRequest;
 import com.godcare.common.dto.ViewProductResponse;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.*;
 
 
@@ -21,51 +19,56 @@ import java.util.*;
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 @Api(tags = "상품 API")
-public class ProductController<V> {
+public class ProductController {
 
     private final ProductService productService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "상품 등록 API ", description = "새로운 상품을 업로드하는 API")
     @PostMapping(path = "")
-    public ResponseEntity<Response<Object>> addProduct(@RequestBody ResisterProductRequest resisterProductRequest) {
+    public Response<ResisterProductResponse> addProduct(@RequestBody ResisterProductRequest resisterProductRequest) {
 
         Product savedProduct = productService.addProduct(Product.toProduct(resisterProductRequest));
 
-        return ApiUtils.success(HttpStatus.CREATED.value(), "상품이 등록되었습니다.", "productId: " + savedProduct.getId());
+        return Response.success(new ResisterProductResponse(savedProduct.getId()));
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "상품 조회 API ", description = "특정 상품 상세조회하는 API")
     @GetMapping(path = "/{product_id}")
-    public ResponseEntity<Response<Object>> viewProduct(@PathVariable(value = "product_id") Long productId) {
+    public Response<ViewProductResponse> viewProduct(@PathVariable(value = "product_id") Long productId) {
         ViewProductResponse viewProductResponse = productService.getProduct(productId);
 
-        return ApiUtils.success(HttpStatus.OK.value(), "상품 상세 페이지 조회에 성공하였습니다.", viewProductResponse);
+        return Response.success(viewProductResponse);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "상품 전체 조회 API ", description = "전체 상품 조회하는 API")
     @GetMapping(path = "")
-    public ResponseEntity<Response<Object>> viewProductList() {
+    public Response<List<ViewProductResponse>> viewProductList() {
 
         List<ViewProductResponse> productList = productService.getAllProducts();
 
-        return ApiUtils.success(HttpStatus.OK.value(), "상품 리스트 전체 조회에 성공하였습니다.", productList);
+        return Response.success(productList);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "상품 수정하는 API ", description = "등록된 상품 정보 수정하는 API")
     @PatchMapping(path = "/{product_id}")
-    public ResponseEntity<Response<Object>> updateProduct(@PathVariable(value = "product_id") Long productId, @RequestBody UpdateProductRequest updateProductRequest) {
+    public Response<String> updateProduct(@PathVariable(value = "product_id") Long productId, @RequestBody UpdateProductRequest updateProductRequest) {
 
         productService.updateProduct(productId, updateProductRequest);
 
-        return ApiUtils.success(HttpStatus.OK.value(), "상품 정보가 정상적으로 업데이트되었습니다.", "productId: " + productId);
+        return Response.success("productId: " + productId);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "상품 삭제 API ", description = "등록된 상품 삭제하는 API")
     @DeleteMapping(path = "/{product_id}")
-    public ResponseEntity<Response<Object>> deleteProduct(@PathVariable(value = "product_id") Long productId) {
+    public Response<String> deleteProduct(@PathVariable(value = "product_id") Long productId) {
 
         productService.deleteProduct(productId);
 
-        return ApiUtils.success(HttpStatus.OK.value(), "상품이 정상적으로 삭제되었습니다.", "productId: " + productId);
+        return Response.success("productId: " + productId);
     }
 }
