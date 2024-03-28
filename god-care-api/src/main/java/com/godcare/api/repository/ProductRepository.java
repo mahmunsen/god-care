075f73cb70e1ed6bca3f6b4427ca0197c8d69379
mahmunsen <tjs339@gmail.com>
2024-03-28 17:38:23 +0900

    #5 [feat] jpa 사용 위한 리포지토리 생성

--- god-care-api/src/main/java/com/godcare/api/repository/ProductRepository.java
+++ god-care-api/src/main/java/com/godcare/api/repository/ProductRepository.java
@@ -1,28 +1,9 @@
 package com.godcare.api.repository;
 
 import com.godcare.api.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
-public class ProductRepository extends ConcurrentHashMap<Long, Product>{
-    private AtomicLong sequence = new AtomicLong(1L);
-    public Product save(Product product) {
-        product.assignId(sequence.getAndIncrement());
-        put(product.getId(), product);
-        return product;
-    }
-    public Product findById(Long productId) {
-         Product found = get(productId);
-        return found;
-    }
-    public void deleteById(Long productId) {
-        remove(productId);
-    }
-
-    public Product saveUpdated(Product product) {
-        put(product.getId(), product);
-        return product;
-    }
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
