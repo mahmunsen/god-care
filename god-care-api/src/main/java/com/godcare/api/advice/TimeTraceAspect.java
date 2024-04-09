fbd489c32ff973ff6d964f5eade75aadda67164c
mahmunsen <tjs339@gmail.com>
2024-04-09 20:08:12 +0900

    #8 [feat] TimeTrace 파일 등 업로드 - 메소드 수행 시간 측정

---
+++ god-care-api/src/main/java/com/godcare/api/advice/TimeTraceAspect.java
@@ -0,0 +1,32 @@
+package com.godcare.api.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Slf4j
+@Component
+@Aspect
+public class TimeTraceAspect {
+    @Pointcut("@annotation(com.godcare.api.advice.annotation.TimeTrace)")
+    private void timeTracePointcut() {
+    }
+    @Around("timeTracePointcut()")
+    public Object traceTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+
+        try {
+            stopWatch.start();
+            return joinPoint.proceed(); // 실제 타겟 호출
+        } finally {
+            stopWatch.stop();
+            log.info("\u001B[36m{} - Total time = {}s\u001B[0m",
+                    joinPoint.getSignature().toShortString(),
+                    stopWatch.getTotalTimeSeconds());
+        }
+    }
+}
