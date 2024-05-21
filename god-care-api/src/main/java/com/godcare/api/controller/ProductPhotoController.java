f79c08b52d1f28da649a69d24b08b41d98d36146
mahmunsen <tjs339@gmail.com>
2024-05-22 04:13:55 +0900

    #11 [refactor] FileService, ProductPhotoController 수정 - 비동기 로직으로 수정

--- god-care-api/src/main/java/com/godcare/api/controller/ProductPhotoController.java
+++ god-care-api/src/main/java/com/godcare/api/controller/ProductPhotoController.java
@@ -8,12 +8,17 @@ import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/product_photos")
 @RequiredArgsConstructor
@@ -21,11 +26,20 @@ import java.util.List;
 public class ProductPhotoController {
 
     private final FileService fileService;
+    private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @Operation(summary = "presigned url 얻는 API ", description = "presigned url 얻는 API")
+//    @PostMapping(path = "/presigned_url")
+//    public CompletableFuture<Response<List<CompletableFuture<FileResponse>>>> getPresignedUrls(
+//            @RequestPart(value = "files") List<MultipartFile> multipartFiles) {
+//        return fileService.getPresignedUrls(multipartFiles).thenApplyAsync((res) -> Response.success(res));
+//    }
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "presigned url 얻는 API ", description = "presigned url 얻는 API")
     @PostMapping(path = "/presigned_url")
-    public Response<List<FileResponse>> getPresignedUrls(
-            @RequestPart(value = "files") List<MultipartFile> multipartFiles) {
-        return Response.success(fileService.getPresignedUrls(multipartFiles));
+    public CompletableFuture<Response<List<FileResponse>>> getPresignedUrls(
+            @RequestPart(value = "files") List<MultipartFile> multipartFiles) throws ExecutionException, InterruptedException {
+        return fileService.getPresignedUrls(multipartFiles).thenApplyAsync((res) -> Response.success(res), threadPoolTaskExecutor);
     }
 }
--- god-care-api/src/main/java/com/godcare/api/service/FileService.java
+++ god-care-api/src/main/java/com/godcare/api/service/FileService.java
@@ -6,27 +6,20 @@ import com.amazonaws.services.s3.Headers;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.godcare.api.advice.annotation.TimeTrace;
 import com.godcare.api.config.BucketProperties;
-import com.godcare.api.entity.FileStatus;
 import com.godcare.api.enums.FilePath;
-import com.godcare.api.exception.FileUploadFailedException;
 import com.godcare.common.dto.FileResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.SdkResponse;
-import software.amazon.awssdk.core.async.AsyncRequestBody;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.model.*;
 
-import java.io.*;
-import java.net.URL;
-import java.nio.ByteBuffer;
 import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -36,20 +29,71 @@ public class FileService {
     private final S3AsyncClient s3AsyncClient;
     private final AmazonS3 amazonS3;
     private final BucketProperties bucketProperties;
+    private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
-    public List<FileResponse> getPresignedUrls(List<MultipartFile> multipartFiles) {
+//    public List<CompletableFuture<FileResponse>> getPresignedUrls(List<MultipartFile> multipartFiles) {
+//        List<CompletableFuture<FileResponse>> fileResponseList = new ArrayList<>();
+//        for (MultipartFile multipartFile : multipartFiles) {
+//            String originalFileName = multipartFile.getOriginalFilename();
+//            String fileName = getNewFileName(originalFileName);
+//            String filePath = FilePath.PRODUCT_DIR.getPath();
+//            String keyName = filePath + "/" + fileName;
+//
+//            CompletableFuture<FileResponse> fileResponse = CompletableFuture.supplyAsync(() ->
+//                            getGeneratePreSignedUrlRequest(bucketProperties.getBucketName(), keyName))
+//                    .thenApplyAsync(generatePresignedUrlRequest -> amazonS3.generatePresignedUrl(generatePresignedUrlRequest), threadPoolTaskExecutor)
+//                    .thenApplyAsync(url -> {
+//                        log.info(threadPoolTaskExecutor.toString());
+//                        log.info("thread: %name", Thread.currentThread().getName());
+//                        log.info("이 일을 처리하고 있는 녀석은?");
+//                        return new FileResponse(originalFileName, fileName, url.toString());
+//                    }, threadPoolTaskExecutor);
+//            fileResponseList.add(fileResponse);
+//        }
+//        return fileResponseList;
+//    }
+
+
+//    public CompletableFuture<List<CompletableFuture<FileResponse>>> getPresignedUrls(List<MultipartFile> multipartFiles) {
+//        CompletableFuture<List<CompletableFuture<FileResponse>>> fileResponseList = CompletableFuture.supplyAsync(() -> {
+//            List<CompletableFuture<FileResponse>> fileRes = new ArrayList<>();
+//            for (MultipartFile multipartFile : multipartFiles) {
+//                String originalFileName = multipartFile.getOriginalFilename();
+//                String fileName = getNewFileName(originalFileName);
+//                String filePath = FilePath.PRODUCT_DIR.getPath();
+//                String keyName = filePath + "/" + fileName;
+//
+//                CompletableFuture<FileResponse> fileResponse = CompletableFuture.supplyAsync(() ->
+//                                getGeneratePreSignedUrlRequest(bucketProperties.getBucketName(), keyName))
+//                        .thenApplyAsync(generatePresignedUrlRequest -> amazonS3.generatePresignedUrl(generatePresignedUrlRequest), threadPoolTaskExecutor)
+//                        .thenApplyAsync(url -> {
+//                            log.info(threadPoolTaskExecutor.toString());
+//                            log.info("thread: %name", Thread.currentThread().getName());
+//                            log.info("이 일을 처리하고 있는 녀석은?");
+//                            return new FileResponse(originalFileName, fileName, url.toString());
+//                        }, threadPoolTaskExecutor);
+//                fileRes.add(fileResponse);
+//            }
+//            return fileRes;
+//        });
+//        return fileResponseList;
+//    }
+
+
+    public CompletableFuture<List<FileResponse>> getPresignedUrls(List<MultipartFile> multipartFiles) throws ExecutionException, InterruptedException {
         List<FileResponse> fileResponseList = new ArrayList<>();
-        for(MultipartFile multipartFile: multipartFiles) {
+        for (MultipartFile multipartFile : multipartFiles) {
             String originalFileName = multipartFile.getOriginalFilename();
             String fileName = getNewFileName(originalFileName);
             String filePath = FilePath.PRODUCT_DIR.getPath();
             String keyName = filePath + "/" + fileName;
-            GeneratePresignedUrlRequest generatePresignedUrlRequest = getGeneratePreSignedUrlRequest(bucketProperties.getBucketName(), keyName);
-            URL url = amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
-            url.toString();
-            fileResponseList.add(new FileResponse(originalFileName, fileName, url));
+
+            CompletableFuture<FileResponse> fileResponse = CompletableFuture.supplyAsync(() -> getGeneratePreSignedUrlRequest(bucketProperties.getBucketName(), keyName).join(), threadPoolTaskExecutor)
+                    .thenApplyAsync(generatePresignedUrlRequest -> amazonS3.generatePresignedUrl(generatePresignedUrlRequest), threadPoolTaskExecutor)
+                    .thenApplyAsync(url -> new FileResponse(originalFileName, fileName, url.toString()), threadPoolTaskExecutor);
+            fileResponseList.add(fileResponse.get());
         }
-        return fileResponseList;
+        return CompletableFuture.completedFuture(fileResponseList);
     }
 
     /**
@@ -58,16 +102,16 @@ public class FileService {
      * @param bucket   버킷 이름
      * @param fileName S3 업로드용 파일 이름
      */
-    private GeneratePresignedUrlRequest getGeneratePreSignedUrlRequest(String bucket, String fileName) {
-        GeneratePresignedUrlRequest generatePresignedUrlRequest =
-                new GeneratePresignedUrlRequest(bucket, fileName)
+    private CompletableFuture<GeneratePresignedUrlRequest> getGeneratePreSignedUrlRequest(String bucket, String fileName) {
+        return CompletableFuture.supplyAsync(() -> new GeneratePresignedUrlRequest(bucket, fileName)
                         .withMethod(HttpMethod.PUT)
-                        .withExpiration(getPreSignedUrlExpiration());
-        generatePresignedUrlRequest.addRequestParameter(
-                Headers.S3_CANNED_ACL,
-                CannedAccessControlList.PublicRead.toString());
-        return generatePresignedUrlRequest;
+                        .withExpiration(getPreSignedUrlExpiration()), threadPoolTaskExecutor)
+                .thenApplyAsync(generatePresignedUrlRequest -> {
+                    generatePresignedUrlRequest.addRequestParameter(Headers.S3_CANNED_ACL, CannedAccessControlList.PublicRead.toString());
+                    return generatePresignedUrlRequest;
+                }, threadPoolTaskExecutor);
     }
+
 
     // presigned url 유효 기간 설정
     private Date getPreSignedUrlExpiration() {
@@ -83,20 +127,20 @@ public class FileService {
         return String.valueOf(Instant.now()) + UUID.randomUUID() + "." + ext;
     }
 
-
-    // 이미지 파일 업데이트
-    public FileResponse updateFile(MultipartFile multipartFile, String filePath, String imgToDelete) {
-
-        amazonS3Client.deleteObject(bucketName + "/" + filePath, imgToDelete);
-
-        String originalFileName = multipartFile.getOriginalFilename();
-        String uploadFileName = getNewFileName(originalFileName);
-        String uploadFileUrl = "";
-
-        ObjectMetadata objectMetadata = getObjectMetadata(multipartFile);
-
-        return getFileResponse(multipartFile, filePath, originalFileName, uploadFileName, uploadFileUrl, objectMetadata);
-    }
+//
+//    // 이미지 파일 업데이트
+//    public FileResponse updateFile(MultipartFile multipartFile, String filePath, String imgToDelete) {
+//
+//        amazonS3Client.deleteObject(bucketName + "/" + filePath, imgToDelete);
+//
+//        String originalFileName = multipartFile.getOriginalFilename();
+//        String uploadFileName = getNewFileName(originalFileName);
+//        String uploadFileUrl = "";
+//
+//        ObjectMetadata objectMetadata = getObjectMetadata(multipartFile);
+//
+//        return getFileResponse(multipartFile, filePath, originalFileName, uploadFileName, uploadFileUrl, objectMetadata);
+//    }
 
     private static ObjectMetadata getObjectMetadata(MultipartFile multipartFile) {
         ObjectMetadata objectMetadata = new ObjectMetadata();
