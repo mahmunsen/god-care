c631a521fb0bf8228c1f0fea1ac8620f4dceee21
mahmunsen <tjs339@gmail.com>
2024-05-28 17:07:52 +0900

    #11 [refactor] ProductController, ProductPhotoController 물품 api 관련 변경사항 업로드

--- god-care-api/src/main/java/com/godcare/api/controller/ProductController.java
+++ god-care-api/src/main/java/com/godcare/api/controller/ProductController.java
@@ -9,13 +9,9 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -28,11 +24,19 @@ public class ProductController {
     private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "상품 등록 API ", description = "새로운 상품을 업로드하는 API")
+    @Operation(summary = "상품 등록 API ", description = "새로운 상품을 등록하는 API")
     @PostMapping(path = "")
-    public CompletableFuture<Response<ResisterProductResponse>> addProduct(@RequestBody ResisterProductRequest resisterProductRequest) {
+    public CompletableFuture<Response<ResisterProductResponse>> addProduct() {
 
-        return productService.addProduct(resisterProductRequest).thenApply((product) -> Response.success(new ResisterProductResponse(product.getId())));
+        return productService.addProduct().thenApply((product) -> Response.success(new ResisterProductResponse(product.getId())));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "이미지 등록 API ", description = "상품의 이미지를 등록하는 API")
+    @PostMapping(path = "/{product_id}/product_photo")
+    public CompletableFuture<Response<UploadPhotoResponse>> uploadImgUrls(@PathVariable(value = "product_id") Long productId, @RequestBody UploadPhotoRequest uploadPhotoRequest) {
+
+        return productService.uploadImgUrls(uploadPhotoRequest, productId).thenApply((product) -> Response.success(new UploadPhotoResponse(product.getId())));
     }
 
     @ResponseStatus(HttpStatus.OK)
@@ -52,11 +56,21 @@ public class ProductController {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "상품 수정하는 API ", description = "등록된 상품 정보 수정하는 API")
-    @PatchMapping(path = "/{product_id}")
-    public Response<UpdateProductResponse> updateProduct(@PathVariable(value = "product_id") Long productId, @ModelAttribute UpdateProductRequest updateProductRequest, @ApiParam(value = "메인 이미지 파일") @RequestPart(value = "mainImg", required = false) MultipartFile multipartFile) {
+    @Operation(summary = "단수 이미지 삭제 API ", description = "이미지 파일 하나씩 삭제 하는 API")
+    @DeleteMapping(path = "/{product_id}/product_photo")
+    public Response<DeletePhotoResponse> deletePhoto(@PathVariable(value = "product_id") Long productId, @RequestBody DeletePhotoRequest deletePhotoRequest) {
 
-        Product product = productService.updateProduct(productId, updateProductRequest, multipartFile);
+        DeletePhotoResponse deletePhotoResponse = productService.deletePhoto(productId, deletePhotoRequest.getImgUrlToDelete());
+
+        return Response.success(deletePhotoResponse);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "상품 정보 등록 완료 하는 API ", description = "등록된 상품 정보 업데이트 하는 API")
+    @PatchMapping(path = "/{product_id}")
+    public Response<UpdateProductResponse> updateProduct(@PathVariable(value = "product_id") Long productId, @RequestBody UpdateProductRequest updateProductRequest) {
+
+        Product product = productService.updateProduct(productId, updateProductRequest);
 
         return Response.success(new UpdateProductResponse(product.getId()));
     }
