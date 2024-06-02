1e067db41eef84952140cdb9ad0dd933c782113d
mahmunsen <tjs339@gmail.com>
2024-06-03 04:03:21 +0900

    #11 [refactor] ProductPhotoRepository 수정 - 단수 이미지 삭제 시, 하드 딜리트 적용

--- god-care-api/src/main/java/com/godcare/api/repository/ProductPhotoRepository.java
+++ god-care-api/src/main/java/com/godcare/api/repository/ProductPhotoRepository.java
@@ -1,8 +1,10 @@
 package com.godcare.api.repository;
 
-import com.godcare.api.entity.Product;
 import com.godcare.api.entity.ProductPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,10 @@ import java.util.Optional;
 
 @Repository
 public interface ProductPhotoRepository extends JpaRepository<ProductPhoto, Long> {
-    Optional<List<ProductPhoto>> findAllByProductAndIsDeletedFalse(Product product);
-    Optional<ProductPhoto> findByImgUrlAndIsDeletedFalseAndProduct(String imgUrl, Product product);
+    Optional<List<ProductPhoto>> findAllByProductId(Long productId);
+    Optional<List<ProductPhoto>> findAllByProductIdAndIsDeletedFalse(Long productId);
+    Optional<ProductPhoto> findByImgUrlAndIsDeletedFalse(String imgUrl);
+    @Modifying
+    @Query(" DELETE from ProductPhoto p WHERE p.id =:id")
+    void deletePhotoById(@Param("id") Long productPhotoId);
 }
